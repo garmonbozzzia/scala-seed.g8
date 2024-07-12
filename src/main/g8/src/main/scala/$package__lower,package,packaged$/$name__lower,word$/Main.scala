af8cd110a6373fa78fe505c8d5b994be03a389a4
Yury Ignatev <yury.ignatev@gmail.com>
2024-07-12 22:47:37 +0700

    fix

--- src/main/g8/src/main/scala/$package__lower,package,packaged$/$name__lower,word$/Main.scala
+++ src/main/g8/src/main/scala/$package__lower,package,packaged$/$name__lower,word$/Main.scala
@@ -12,6 +12,6 @@ object Main extends ZIOAppDefault {
     for {
       _    <- printLine("Hello! What is your name?")
       name <- readLine
-      _    <- printLine(s"Hello, ${name}, welcome to ZIO!")
+      _    <- printLine(s"Hello, \${name}, welcome to ZIO!")
     } yield ()
 }
